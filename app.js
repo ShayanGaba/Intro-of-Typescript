1855e40b700c44a669a35c3773c55e9cf960331e
ShayanGaba <451shayan@gmail.com>
2024-07-01 20:51:11 +0500

    Introduction of Typescript

---
+++ app.js
@@ -0,0 +1,34 @@
+"use strict";
+// console.log("Hello World");
+
+
+// let user = 'Shayan';
+// user = 'Ali';
+
+
+// let num = 7;
+// num = 5;
+
+
+// let user: string = 'Shayan';
+// user = 'Ali';
+
+
+// let num: number = 4;
+// num = 9;
+// let user: string | number = 'Shayan';
+// user = 3;
+
+
+// let fruits: string[] = ['Apple', 'Banana'];
+// fruits = ['Grapes'];
+
+
+// let mixArr: (string | number)[] = [1, 2];
+// mixArr = ['Hello'];
+
+//Any is use for any datatype that variable accept and not show error
+// let user: any;
+// user = true;
+// user = 2;
+// user = 'Shayan';
